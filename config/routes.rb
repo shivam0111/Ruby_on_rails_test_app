d6206abdfffa790e274d6d081cd55867b465c133
Shivam Arora <shivam.arora@engagedly.com>
2022-09-23 11:27:47 +0530

    showing articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :articles
+ #it gives a whole bunch of routes and links
   root 'pages#home'
   get 'about', to:'pages#about'
+  resources :articles
 end
